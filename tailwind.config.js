db5ba71ee5647ef144ada85d4d0a77b5ce9f9689
aifeifei798 <aifeifei798@outlook.com>
2025-09-29 22:39:22 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
 	"./src/pages/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['"Noto Serif SC"', 'serif'],
+        sans: ['"Noto Sans SC"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
